bc84b71e2cfdd4da70541bdd2cdaa05a63945010
Giovanna Moeller <giovannamoeller@MacBook-Air-de-Giovanna.local>
2020-07-27 23:08:13 -0300

    Trabalhando com promessas no AJAX

--- javascript/ajax/main.js
+++ javascript/ajax/main.js
@@ -8,3 +8,31 @@ xhr.onreadystatechange = () => {
         console.log(JSON.parse(xhr.responseText))
     }
 }
+
+// Promises
+
+var minhaPromise = function() {
+    return new Promise(function(resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://api.github.com/users/giovannamoeller');
+        xhr.send();
+        xhr.onreadystatechange = () => {
+            if(xhr.readyState == 4) { // resposta voltou
+               if(xhr.status == 200) {
+                   resolve(JSON.parse(xhr.responseText));
+               } else {
+                   reject('Erro na requisição');
+               }
+            }
+        }
+    })
+}
+
+minhaPromise()
+    .then(response => { // resolve invoca o then
+        console.log(response)
+    })
+    .catch(error => { // reject invoca o catch
+        console.log(error)
+    })
+// console.log(resultado); // não espera esse resultado
