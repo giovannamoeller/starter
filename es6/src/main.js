63cf0bacfb70570020791162e63d35e21c9962dd
Giovanna Moeller <giovannamoeller@MacBook-Air-de-Giovanna.local>
2020-07-29 16:37:58 -0300

    Utilizando axios com a API do github

--- es6/src/main.js
+++ es6/src/main.js
@@ -2,8 +2,12 @@ import axios from 'axios';
 
 class API {
     static async getUserInfo(username) {
-        const response = await axios.get(`https://api.github.com/users/${username}`);
-        console.log(response);
+        try {
+            const response = await axios.get(`https://api.github.com/users/${username}`);
+            console.log(response);
+        } catch (err) {
+            console.warn('Erro na API')
+        }
     }
 }
 
