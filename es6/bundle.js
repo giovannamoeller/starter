d215bd76fcbf19e34584f91c5cab95804b8bdfcc
Giovanna Moeller <giovannamoeller@MacBook-Air-de-Giovanna.local>
2020-07-28 22:49:59 -0300

    Classes e Herança

--- es6/bundle.js
+++ es6/bundle.js
@@ -1,31 +1,75 @@
 "use strict";
 
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-var ToDoList = /*#__PURE__*/function () {
+var List = /*#__PURE__*/function () {
+  function List() {
+    _classCallCheck(this, List);
+
+    this.data = [];
+  }
+
+  _createClass(List, [{
+    key: "add",
+    value: function add(data) {
+      this.data.push(data);
+      console.log(this.data);
+    }
+  }]);
+
+  return List;
+}();
+
+var ToDoList = /*#__PURE__*/function (_List) {
+  _inherits(ToDoList, _List);
+
+  var _super = _createSuper(ToDoList);
+
   function ToDoList() {
+    var _this;
+
     _classCallCheck(this, ToDoList);
 
-    this.toDo = [];
+    _this = _super.call(this); // chamar o constructor da classe pai
+
+    _this.usuario = 'Giovanna';
+    return _this;
   }
 
   _createClass(ToDoList, [{
-    key: "addToDo",
-    value: function addToDo() {
-      this.toDo.push('Novo item');
-      console.log(this.toDo);
+    key: "mostraUsuario",
+    value: function mostraUsuario() {
+      console.log(this.usuario);
     }
   }]);
 
   return ToDoList;
-}();
+}(List);
 
 var MinhaLista = new ToDoList();
 
 document.getElementById('novoToDo').onclick = function () {
-  MinhaLista.addToDo();
+  MinhaLista.add('Novo todo');
 };
+
+MinhaLista.mostraUsuario(); // método static - não enxerga o resto da classe (normalmente não usa com constructor)
