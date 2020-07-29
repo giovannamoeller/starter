8b4c30badf442f4fcce01aa965d2b7cf78a5183b
Giovanna Moeller <giovannamoeller@MacBook-Air-de-Giovanna.local>
2020-07-28 23:59:49 -0300

    Sintaxe curta de objetos

---
+++ es6/conceitos/object-short-syntax.js
@@ -0,0 +1,16 @@
+const nome = 'Giovanna';
+const idade = 19;
+
+/* const usuario = {
+    nome: nome,
+    idade: idade,
+    status: 'Estudante'
+} */
+
+const usuario = { // sintaxe curta
+    nome,
+    idade,
+    status: 'Estudante'
+}
+
+console.log(usuario);
