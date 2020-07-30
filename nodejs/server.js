da96d7303378229c0358e7702541672171d891dd
Giovanna Moeller <giovannamoeller@MacBook-Air-de-Giovanna.local>
2020-07-29 23:23:53 -0300

    Conectando banco de dados

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/nodeAPI', { useNewUrlParser: true, useUnifiedTopology: true }); //nodeAPI é o nome da database
 
 app.get('/', (req, res) => {
     return res.send('Bem - vindo!');
