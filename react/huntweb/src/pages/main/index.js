aacfd259f57b17b632b5246a4b7eb7c8b57a223e
Giovanna Moeller <giovannamoeller@MacBook-Air-de-Giovanna.local>
2020-07-30 18:46:50 -0300

    Utilizando variáveis

--- react/huntweb/src/pages/main/index.js
+++ react/huntweb/src/pages/main/index.js
@@ -3,17 +3,26 @@ import api from '../../services/api'
 import React, { Component } from "react";
 
 export default class Main extends Component {
+    state = {
+        products: [],
+    }
     componentDidMount() { // ciclo de vida = executado assim que o componente for mostrado em tela
         this.loadProducts();
     }
 
     loadProducts = async () => { // função normal apenas para função do React, as nossas são arrow
         const response = await api.get('/products');
-        console.log(response.data.docs);
-
         
+        this.setState({ products: response.data.docs });
+
     }
     render() {
-        return <h1>Oii</h1>
+        return (
+            <div className="product-list">
+                {this.state.products.map(product => (
+                    <h2 key={product.id}>{product.title}</h2>
+                ))}
+            </div>
+        )
     }
 }
