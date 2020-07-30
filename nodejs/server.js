8acbc5d6052c02872a97a4a5f71bdad9ed205efe
Giovanna Moeller <giovannamoeller@MacBook-Air-de-Giovanna.local>
2020-07-30 00:28:09 -0300

    Instalando CORS

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const requireDir = require('require-dir');
 
 // Iniciando o app
 const app = express();
 app.use(express.json()); // permitir com que envie dados no formato JSON
+app.use(cors()); // API pode ser acessada publicamente
 
 // Conectando database
 mongoose.connect('mongodb://localhost:27017/nodeAPI', { useNewUrlParser: true, useUnifiedTopology: true }); //nodeAPI é o nome da database
