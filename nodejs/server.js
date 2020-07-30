3c0e9bbe19c96388baa5f3bbe43aa6fddea8011d
Giovanna Moeller <giovannamoeller@MacBook-Air-de-Giovanna.local>
2020-07-29 23:45:04 -0300

    Criando o primeiro registro com mongoose

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
+// Iniciando o app
 const app = express();
 
+// Conectando database
 mongoose.connect('mongodb://localhost:27017/nodeAPI', { useNewUrlParser: true, useUnifiedTopology: true }); //nodeAPI é o nome da database
 
+requireDir('./src/models/');
+// Criando primeiro model
+
+const Product = mongoose.model('Product');
+
 app.get('/', (req, res) => {
+    Product.create({ 
+        title: 'iPhone',
+        description: 'iPhone 11 128 gb',
+        url: 'http://apple.com'
+    })
     return res.send('Bem - vindo!');
 });
 
